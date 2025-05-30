fb3482700ff37a8e724312e819852e766cd64893
Umutt <umutulay1907@gmail.com>
2025-05-29 21:08:36 -0500

    adding files

--- deneme.py
+++ deneme.py
@@ -2,4 +2,4 @@ from ultralytics import YOLO
 
 model = YOLO("runs/detect/train2/weights/best.pt")
 
-results = model("bouldering_route.png", save=True, conf=0.25)
+results = model("trial.png", save=True, conf=0.25)
