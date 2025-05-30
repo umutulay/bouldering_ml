5a18aff8b5356aec2d03932a405435caf922ffa2
Umutt <umutulay1907@gmail.com>
2025-05-29 21:33:24 -0500

    full files

--- deneme.py
+++ deneme.py
@@ -2,4 +2,4 @@ from ultralytics import YOLO
 
 model = YOLO("runs/detect/train2/weights/best.pt")
 
-results = model("trial.png", save=True, conf=0.25)
+results = model("route1.jpg", save=True, conf=0.25)
